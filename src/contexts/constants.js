0f993fe69eb6718ad3f256c27bc046934efa2cf0
Nhan Huynh <httnhan99@gmail.com>
2023-06-19 22:25:01 +0700

    update

--- src/contexts/constants.js
+++ src/contexts/constants.js
@@ -1,7 +1,7 @@
-export const apiUrl =
-	process.env.NODE_ENV !== 'production'
-		? 'http://localhost:5000/api'
-		: 'https://sleepy-inlet-56101.herokuapp.com/api'
+export const apiUrl = 'https://learn-server-psi.vercel.app//api'
+	// process.env.NODE_ENV === 'production'
+	// 	? 'learn-server-q0sgqe6dt-huynhnhan99.vercel.app/api'
+	// 	: 'http://localhost:5000/api' 
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern'
 
